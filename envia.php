116fdd3b542867cbde3dd32002284e90eec4c8f2
matheushenrique <167774845+Matheus122332@users.noreply.github.com>
2024-10-14 19:28:04 -0300

    Add files via upload

---
+++ envia.php
@@ -0,0 +1,19 @@
+<?php
+    $nome = addcslashes($_POST['nome']);
+    $email = addcslashes($_POST['email']);
+    $celular = addcslashes($_POST['celular']);
+    $mensagem = addcslashes($_POST['mensagem']);
+
+    $para = "matheushenriquebrito1999@gmail.com";
+    $assunto = "Coleta de dados - AGÊNCIA MHT";
+
+    $corpo = "Nome: ".$nome."\n"."E-mail".$email."\n"."Celular".$celular."\n"."Mensagem".$mensagem;
+
+    $cabeca = "From: matheushenriquebrito1999@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if (mail($para, $assunto, $corpo, $cabeca)) {
+        echo("E-mail enviado com sucesso!");
+    }else{
+        echo("Houve um erro ao enviar o email!");
+    }
+?>
