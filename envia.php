fc3381984a413a80738fc0592f04e1cf2438158d
matheushenrique <167774845+Matheus122332@users.noreply.github.com>
2024-10-15 09:06:00 -0300

    Update envia.php

--- envia.php
+++ envia.php
@@ -1,34 +1,28 @@
 <?php
-  //Variáveis
-  $nome = $_POST['nome'];
-  $email = $_POST['email'];
-  $celular = $_POST['celular'];
-  $mensagem = $_POST['mensagem'];
-  $data_envio = date('d/m/Y');
-  $hora_envio = date('H:i:s');
-
-  //Campo E-mail
-  $arquivo = "
-    <html>
-      <p><b>Nome: </b>$nome</p>
-      <p><b>E-mail: </b>$email</p>
-      <p><b>Celular: </b>$celular</p>
-      <p><b>Mensagem: </b>$mensagem</p>
-      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
-    </html>
-  ";
-  
-  //Emails para quem será enviado o formulário
-  $destino = "matheushenriquebrito1999@gmail.com";
-  $assunto = "Coleta de dados - AGÊNCIA MHT";
-
-  //Este sempre deverá existir para garantir a exibição correta dos caracteres
-  $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
-
-  //Enviar
-  mail($destino, $assunto, $arquivo, $headers);
-  
-  echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $celular = addslashes($_POST['celular']);
+    $mensagem = addslashes($_POST['mensagem']);
+    
+    $para = "matheushenriquebrito1999@gmail.com";
+    $assunto = "Coleta de dados - AGÊNCIA MHT";
+    $corpo = "Nome: " . $nome . "\n" . 
+             "E-mail: " . $email . "\n" . 
+             "Celular: " . $celular . "\n" . 
+             "Mensagem: " . $mensagem;
+    
+    $cabeca = "From: matheushenrisuporte@gmail.com" . "\r\n" .
+              "Reply-To: " . $email . "\r\n" .
+              "X-Mailer: PHP/" . phpversion();
+    
+    if (mail($para, $assunto, $corpo, $cabeca)) {
+        echo "E-mail enviado com sucesso!";
+    } else {
+        echo "Houve um erro ao enviar o e-mail!";
+    }
+} else {
+    echo "Método de requisição inválido!";
+}
 ?>
+
