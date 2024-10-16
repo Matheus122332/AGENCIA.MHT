cf6a2850de16fe4c8879ebcf832ca30e716ee349
matheushenrique <167774845+Matheus122332@users.noreply.github.com>
2024-10-16 11:56:59 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,53 @@
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+document.getElementById('celular').addEventListener('input', function(e) {
+    let x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
+    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+});
+document.getElementById('email').addEventListener('input', function() {
+    const email = this.value;
+    const emailError = document.getElementById('email-error');
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailPattern.test(email)) {
+        emailError.style.display = 'none';
+        this.setCustomValidity('');
+    } else {
+        emailError.style.display = 'inline';
+        this.setCustomValidity('Por favor, insira um e-mail válido.');
+    }
+});
+// Mostrar o botão ao rolar a página
+window.onscroll = function() {
+    let button = document.getElementById("back-to-top");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+// Rolar para o topo ao clicar no botão
+document.getElementById("back-to-top").addEventListener("click", function() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
+});
